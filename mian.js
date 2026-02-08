9110719d04df47968280a87e2198b73bf0b82b83
Naveed Mian <naveedmian0342@gmail.com>
2026-02-08 23:25:04 +0500

    batter

--- mian.js
+++ mian.js
@@ -12,3 +12,28 @@ div.style.textAlign = "center"
  div.appendChild(addText)
 
  document.body.appendChild(div)
+  
+ function addLanguage(langName) {
+   const li =  document.createElement("li");
+   li.innerHTML = `${langName}`
+   document.querySelector(".language").appendChild(li)
+ }
+ addLanguage("java")
+ addLanguage("php")
+
+ function addOptiLanguage(langName) {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(langName))
+     document.querySelector(".language").appendChild(li)
+ }
+ addOptiLanguage("c++")
+
+const secondLang = document.querySelector("li:nth-child(2)")
+
+ const newLi = document.createElement("li")
+ newLi.textContent = "Mojo"
+ secondLang.replaceWith(newLi)
+ const firstLang = document.querySelector("li:first-child")
+ firstLang.outerHTML = "<li>phy<li>"
+ const lastLang = document.querySelector("li:last-child")
+ lastLang.remove()
